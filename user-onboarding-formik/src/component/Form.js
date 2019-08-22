0dccc476148665daba67ea200f323899db9d68af
amlane <twelvexstring@gmail.com>
2019-08-22 15:37:24 -0500

    errors msgs working besides terms of service

--- user-onboarding-formik/src/component/Form.js
+++ user-onboarding-formik/src/component/Form.js
@@ -1,18 +1,29 @@
 import React from "react";
 import { withFormik, Form, Field } from "formik";
+import * as Yup from "yup";
 
-function UserForm() {
+function UserForm({ errors, touched, values, status }) {
   return (
     <div>
       <Form>
         <Field name="name" type="text" placeholder="name" />
+        {touched.name && errors.name && <p className="error">{errors.name}</p>}
         <Field name="email" type="email" placeholder="email" />
+        {touched.email && errors.email && (
+          <p className="error">{errors.email}</p>
+        )}
         <Field name="password" type="password" placeholder="password" />
+        {touched.password && errors.password && (
+          <p className="error">{errors.password}</p>
+        )}
         <label className="checkbox-container">
           Terms of Service
-          <Field type="checkbox" name="terms" />
+          <Field type="checkbox" name="terms" checked={values.terms} />
           <span className="checkmark" />
         </label>
+        {touched.terms && errors.terms && (
+          <p className="error">{errors.terms}</p>
+        )}
         <button type="submit">Press here</button>
       </Form>
     </div>
@@ -25,9 +36,15 @@ const FormikForm = withFormik({
       name: name || "",
       email: email || "",
       password: password || "",
-      terms: terms || ""
+      terms: terms || false
     };
-  }
+  },
+  validationSchema: Yup.object().shape({
+    name: Yup.string().required("Oh no! You forgot your name!"),
+    email: Yup.string().required("Oh no! You forgot your email!"),
+    password: Yup.string().required("Oh no! You forgot your password!"),
+    terms: Yup.boolean().required("Please check terms of service.")
+  })
 })(UserForm);
 
 export default FormikForm;
